b9241f4f20a08cee9b68bf13dd44e129a3af5c38
Chien <chiendttp@gmail.com>
2016-12-23 20:59:32 +0700

    upload file

---
+++ batdongsan/index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Stylish Portfolio - Start Bootstrap Theme</title>
+
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom CSS -->
+        <link href="css/style.css" rel="stylesheet">
+
+        <!-- Custom Fonts -->
+        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+      
+
+    </head>
+
+    <body>
+
+        <!-- Navigation -->
+     
+        <nav class="navbar navbar-default navbar-fixed-top" style="background-color: #222; opacity:0.5">        
+             <div class="container">       
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>                        
+                    </button>
+                    <a class="navbar-brand" href="#">ABOUT ME</a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                  <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#index.php">TRANG CHỦ</a></li>
+                    <li><a href="#">GIỚI THIỆU</a></li>                    
+                    <li><a href="lienhe.php">LIÊN HỆ</a></li>
+                    
+                  </ul>
+                </div>
+            </div>
+
+        </nav>
+
+        <!-- Header -->
+        <header id="top" class="header">
+            <div class="text-vertical-center">
+                <img class="img-portfolio img-responsive" src="img/bg.jpg" alt="1" style="height: 60%">
+                
+            </div>
+        </header>
+
+        <!-- Portfolio -->
+    <section id="portfolio" class="portfolio">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1 text-center">
+                    <h2>TIN NỔI BẬC</h2>
+                    <hr class="small">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="portfolio-item">
+                                <a href="#">
+                                    <img class=" img-responsive" src="img/1.jpg">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="portfolio-item">
+                                <a href="#">
+                                    <img class=" img-responsive" src="img/2.jpg">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="portfolio-item">
+                                <a href="#">
+                                    <img class="img-responsive" src="img/3.jpg">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="portfolio-item">
+                                <a href="#">
+                                    <img class="img-responsive" src="img/4.jpg">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="portfolio-item">
+                                <a href="#">
+                                    <img class="img-responsive" src="img/5.jpg">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="portfolio-item">
+                                <a href="#">
+                                    <img class="img-responsive" src="img/6.jpg">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="portfolio-item">
+                                <a href="#">
+                                    <img class="img-responsive" src="img/7.jpg">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="portfolio-item">
+                                <a href="#">
+                                    <img class="img-responsive" src="img/8.jpg">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="portfolio-item">
+                                <a href="#">
+                                    <img class="img-responsive" src="img/9.jpg">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="portfolio-item">
+                                <a href="#">
+                                    <img class="img-responsive" src="img/10.jpg">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="portfolio-item">
+                                <a href="#">
+                                    <img class="img-responsive" src="img/11.jpg">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="portfolio-item">
+                                <a href="#">
+                                    <img class="img-responsive" src="img/12.jpg">
+                                </a>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <!-- /.row (nested) -->
+                    <a href="#" class="btn btn-dark">View More Items</a>
+                </div>
+                <!-- /.col-lg-10 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+    <div class="footer">
+        <div class="col-lg-12 col-lg-offset-1 text-center">       
+                <div class="col-md-6">
+                    <div class="portfolio-item">
+                        <p>Công ty TNHH MTV Chiến Thắng</p>
+                        <p>92 Hoàng Ngân, Cẩm Lệ, Đà Nẵng</p>
+                        <p>Fax: 08 5113.266.378</p>
+                        
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="portfolio-item">
+                        <p>Công ty TNHH MTV Chiến Thắng</p>
+                        <p>92 Hoàng Ngân, Cẩm Lệ, Đà Nẵng</p>
+                        <p>Fax: 08 5113.266.378</p>
+                    </div>
+                      
+                </div>
+        </div>
+
+    </div>
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+
+    </body>
+</html>
